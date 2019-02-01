7975a50cab721415ab385944e4b58fb7d512d0e0
Kurt Galicha <kurtg4@109d25pf0fhf8j7.local>
2019-02-01 12:25:04 -0700

    Initial Commit

---
+++ Github Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Test
+//
+//  Created by Kurt Galicha on 2019-02-01.
+//  Copyright © 2019 Kurt Galicha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
